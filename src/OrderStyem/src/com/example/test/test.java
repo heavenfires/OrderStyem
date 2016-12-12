1aa3ace65c0fb7e6d34d99d5514f951fbee20c36
923372148 <aa1435889856@sina.com>
2016-12-13 07:56:03 +0800

    If applied，this commit will unit testing

--- src/OrderStyem/src/com/example/test/test.java
+++ src/OrderStyem/src/com/example/test/test.java
@@ -1,11 +1,14 @@
 package com.example.test;
 
-import com.example.orderstyem.Vdata;   
+import com.example.orderstyem.Vdata;  
+import com.example.orderstyem.Userdata;
 import android.test.AndroidTestCase;  
 
 public class test extends AndroidTestCase{
 	private String Vname;
 	private Integer Vprice; // ¼Û¸ñ
+	private String username;  
+    private String password; 
 	
 	public String testgetVname() throws Exception{  
 		 Vdata vdata = new Vdata();  
@@ -26,4 +29,29 @@ public class test extends AndroidTestCase{
 		 Vdata vdata = new Vdata();  
 		 vdata.setVprice( Vprice);  
 	    }  	
+	 
+	 public boolean testgetusername() throws Exception{  
+		 if("root".equals(username)){
+			 return true;
+		 }
+		return false;
+	    }  	
+	 
+	 public void testsetVusername() throws Exception{  
+		 Userdata userdata = new Userdata();  
+		 userdata.setVusername(username);  
+	    }  	
+	 
+	 public boolean testgetpassword() throws Exception{  
+		 if("admin".equals(password)){
+			 return true;
+		 }
+		return false;
+	    }  	
+	 
+	 public void testsetVpassword() throws Exception{  
+		 Userdata userdata = new Userdata();  
+		 userdata.setVpassword( password);  
+	    }  	
+	 
 }
